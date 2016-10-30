2936550d71abf29432e7e0c383be71c55b46419d
Colin Williams <colinw@nc.rr.com>
2016-10-30 11:09:20 -0400

    new files

--- include/CConv.h
+++ include/CConv.h
@@ -14,4 +14,72 @@ namespace CConv {
   }
 }
 
+namespace CConv {
+  template<class T>
+  std::vector<T> toVector(const T *a, uint n) {
+    std::vector<T> v;
+
+    v.resize(n);
+
+    for (uint i = 0; i < n; ++i)
+      v[i] = a[i];
+
+    return v;
+  }
+
+  template<class T, class U>
+  std::vector<T> toVector(const U *a, const U *b, uint n) {
+    std::vector<T> v;
+
+    v.resize(n);
+
+    for (uint i = 0; i < n; ++i)
+      v[i] = T(a[i], b[i]);
+
+    return v;
+  }
+
+  template<class T>
+  std::vector<T> toVector(const T &t1) {
+    std::vector<T> v;
+
+    v.push_back(t1);
+
+    return v;
+  }
+
+  template<class T>
+  std::vector<T> toVector(const T &t1, const T &t2) {
+    std::vector<T> v;
+
+    v.push_back(t1);
+    v.push_back(t2);
+
+    return v;
+  }
+
+  template<class T>
+  std::vector<T> toVector(const T &t1, const T &t2, const T &t3) {
+    std::vector<T> v;
+
+    v.push_back(t1);
+    v.push_back(t2);
+    v.push_back(t3);
+
+    return v;
+  }
+
+  template<class T>
+  std::vector<T> toVector(const T &t1, const T &t2, const T &t3, const T &t4) {
+    std::vector<T> v;
+
+    v.push_back(t1);
+    v.push_back(t2);
+    v.push_back(t3);
+    v.push_back(t4);
+
+    return v;
+  }
+}
+
 #endif
